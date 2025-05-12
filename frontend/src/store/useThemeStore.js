800bd951e7f8e072a3dd61b1d773cbcd47dcfee8
waris <amwaris2005@gmail.com>
2025-05-12 21:04:46 +0530

    first commit

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,9 +1,9 @@
 import { create } from "zustand";
 
 export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("streamify-theme") || "coffee",
+  theme: localStorage.getItem("QuickConnect -theme") || "coffee",
   setTheme: (theme) => {
-    localStorage.setItem("streamify-theme", theme);
+    localStorage.setItem("QuickConnect -theme", theme);
     set({ theme });
   },
 }));
